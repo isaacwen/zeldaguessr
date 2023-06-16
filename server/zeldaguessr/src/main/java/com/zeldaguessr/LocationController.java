caccf7f4b9aaa5838594ac16f47d4a5ec8a6363d
isaacwen <wen.isaac11@gmail.com>
2023-06-16 10:50:13 -0400

    added api req for random locations

--- server/zeldaguessr/src/main/java/com/zeldaguessr/LocationController.java
+++ server/zeldaguessr/src/main/java/com/zeldaguessr/LocationController.java
@@ -9,15 +9,26 @@ import org.springframework.web.bind.annotation.*;
 public class LocationController {
     @Autowired
     private LocationRepository locationRepository;
+    private final Integer MAX_RANDOM = 5;
 
-    @PostMapping(path = "/add")
+    @PostMapping("/add")
     public @ResponseBody String addNewLocation(@RequestBody Location loc) {
         locationRepository.save(loc);
         return "Saved";
     }
 
-    @GetMapping(path = "/all")
+    @GetMapping("/all")
     public @ResponseBody Iterable<Location> getAllLocations() {
         return locationRepository.findAll();
+    }
+
+    @GetMapping("/random/{numRandom}")
+    public @ResponseBody Iterable<Location> getRandomLocations(@PathVariable Integer numRandom) {
+        if (numRandom <= 0) {
+            numRandom = 1;
+        } else if (numRandom > MAX_RANDOM) {
+            numRandom = MAX_RANDOM;
+        }
+        return locationRepository.findRandomLocations(numRandom);
     }
 }
--- server/zeldaguessr/src/main/java/com/zeldaguessr/LocationRepository.java
+++ server/zeldaguessr/src/main/java/com/zeldaguessr/LocationRepository.java
@@ -1,6 +1,10 @@
 package com.zeldaguessr;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface LocationRepository extends CrudRepository<Location, Integer> {
+    @Query(value = "SELECT * FROM location ORDER BY RAND() LIMIT :numRandom", nativeQuery = true)
+    Iterable<Location> findRandomLocations(@Param("numRandom") Integer numRandom);
 }
